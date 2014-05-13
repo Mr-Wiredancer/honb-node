b8805259780209ab97a086fa3a53479e96c6b7ac
Mr. Wiredancer <jiahao@dxhackers.com>
2014-05-13 13:01:22 +0800

    check url

--- app.js
+++ app.js
@@ -146,9 +146,15 @@ app.post('/weixin', [wechatHelper(APPID, APPSECRET, TOKEN)], function(req, res){
   // } else if(msg.isLocation()){
     // var x = parseFloat(msg['Location_Y']), y = parseFloat(msg['Location_X']); //腾讯地图跟微信的坐标信息貌似反过来了
     //TODO:
-    msg.sendResponseMessage(req, res, 'news',
-      getShops(113, 22)
-    );
+
+
+    // msg.sendResponseMessage(req, res, 'news',
+    //   getShops(113, 22)
+    // );
+    msg.sendResponseMessage(req, res, 'text', {
+      content: getShops(113, 22)[0].url;
+
+    });
   } 
 
   res.send('');
